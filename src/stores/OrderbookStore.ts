ce3493fa40ce52a1708250f31e5a8b5b4e512f01
Agustin Aguilar <Agusxrun@gmail.com>
2021-09-14 18:39:28 +0000

    Re-broadcast once per hour

--- src/stores/OrderbookStore.ts
+++ src/stores/OrderbookStore.ts
@@ -17,7 +17,7 @@ export type StoredOrder = {
   lastSeen: number
 }
 
-export const REBROADCAST_WINDOW = 24 * 60 * 60 * 1000
+export const REBROADCAST_WINDOW = 1 * 60 * 60 * 1000
 
 export class OrderbookStoreClass {
   // If we want to persist all the known orders maybe we should use IndexedDB
