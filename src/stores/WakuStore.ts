762911762f68c99f59f83157ee909bc6e3d1467b
Agustin Aguilar <Agusxrun@gmail.com>
2021-09-16 11:33:46 +0000

    Waku v0.13

--- src/stores/WakuStore.ts
+++ src/stores/WakuStore.ts
@@ -1,4 +1,5 @@
-import { getBootstrapNodes, Waku, WakuMessage } from 'js-waku'
+import axios from 'axios'
+import { Waku, WakuMessage } from 'js-waku'
 import { observable } from 'micro-observables'
 
 import { Store } from './'
@@ -15,6 +16,46 @@ type WakuObserver = {
 export type WakuCallback<T> = {
   callback: (event: T) => void
   isEvent: (cand: Partial<T>) => cand is T
+}
+
+/*
+  This code downloads the bootstrap list of nodes for Waku,
+  it should remain on their lib, but it has a bug.
+  I copy-pasted it here until it can be fixed upstream, we are ddosing Waku and this will help relieve it.
+*/
+const DefaultWantedNumber = 2
+async function getBootstrapNodes(path = ['fleets', 'wakuv2.prod', 'waku-websocket'], url = 'https://fleets.status.im/', wantedNumber = DefaultWantedNumber) {
+    if (wantedNumber <= 0) {
+        return []
+    }
+    const res = await axios.get(url, {
+        headers: { 'Content-Type': 'application/json' },
+    })
+
+    let nodes = res.data
+    for (const prop of path) {
+        if (nodes[prop] === undefined) {
+            throw new Error(`Failed to retrieve bootstrap nodes: ${prop} does not exist on ${JSON.stringify(nodes)}`)
+        }
+        nodes = nodes[prop]
+    }
+    if (Array.isArray(nodes)) {
+        return getPseudoRandomSubset(nodes, wantedNumber)
+    }
+
+    if (typeof nodes === 'string') {
+        return [nodes]
+    }
+    if (typeof nodes === 'object') {
+        nodes = Object.values(nodes)
+        return getPseudoRandomSubset(nodes, wantedNumber)
+    }
+
+    throw new Error(`Failed to aaaa retrieve bootstrap nodes: response format is not supported: ${JSON.stringify(nodes)}`)
+}
+
+function getPseudoRandomSubset<T>(values: Array<T>, wantedNumber: number): Array<T> {
+  return values.map((v) => ({ v, r: Math.random() })).sort((a, b) => a.r - b.r).slice(0, wantedNumber).map((v) => v.v)
 }
 
 export class WakuStore {
@@ -39,8 +80,9 @@ export class WakuStore {
 
       await Promise.all(bootNodes.map(async (n: string) => {
         try {
-          console.debug("connect to", n)
+          console.info("connect to", n)
           await waku.dial(n)
+          console.info("connected to", n)
         } catch (e) {
           console.warn("error connecting to peer", n, e)
         }
